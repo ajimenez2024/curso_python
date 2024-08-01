adf2ce0b033aff73c65982d381721c393a1d4164
ajimenez2024 <missestudiosdeprogramacion@gmail.com>
2024-08-01 17:59:25 -0500

    cap

--- modulo_01/capitulo_04_input/Ejercicio_4.1.py
+++ modulo_01/capitulo_04_input/Ejercicio_4.1.py
@@ -2,3 +2,7 @@
  
 # Cree la variable 'Nombre', almacene mediante un 'input()' el nombre de su asignatura favorita
 # e imprima el resultado
+
+Nombre=input("¿Cuál es tu asignatura favorita?")
+print(Nombre)
+
--- modulo_01/capitulo_04_input/Ejercicio_4.2.py
+++ modulo_01/capitulo_04_input/Ejercicio_4.2.py
@@ -2,3 +2,9 @@
  
 # Cree la variable 'Ejercicio', almacene mediante un 'input()' las horas semanales que haces de ejercicio,
 # conviertalo a 'int()' e imprima el resultado.
+
+Ejercicio=input("ingrese la cantidad de horas semanales que hace ejercicio ")
+Ejercicio=int(Ejercicio)
+print("Usted hace",Ejercicio,"horas semanales de ejercicio")
+
+# profe, le coloqué ese mensaje porque lo ví muy escueto y se veía feo cuando lo ejecutaba.
